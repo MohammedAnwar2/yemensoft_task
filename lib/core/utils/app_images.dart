0fd6e23d62b550cb46717e330c54360a8b0e1bf2
MohammedAnwar2 <moanbm123@gmail.com>
2025-04-25 22:51:22 +0530

    add required images and fonts

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,4 +1,37 @@
 // ignore_for_file: prefer_single_quotes
 class AppImages {
   AppImages._();
+  
+  /// Assets for imagesArabicFlag
+  /// assets/images/arabic_flag.svg
+  static const String imagesArabicFlag = "assets/images/arabic_flag.svg";
+
+  /// Assets for imagesDelivery
+  /// assets/images/delivery.svg
+  static const String imagesDelivery = "assets/images/delivery.svg";
+
+  /// Assets for imagesDeliveryman
+  /// assets/images/deliveryman.png
+  static const String imagesDeliveryman = "assets/images/deliveryman.png";
+
+  /// Assets for imagesDeliveryman2
+  /// assets/images/deliveryman_2.svg
+  static const String imagesDeliveryman2 = "assets/images/deliveryman_2.svg";
+
+  /// Assets for imagesEmptyorder
+  /// assets/images/emptyorder.svg
+  static const String imagesEmptyorder = "assets/images/emptyorder.svg";
+
+  /// Assets for imagesEnglishFlag
+  /// assets/images/english_flag.svg
+  static const String imagesEnglishFlag = "assets/images/english_flag.svg";
+
+  /// Assets for imagesLanguage
+  /// assets/images/language.svg
+  static const String imagesLanguage = "assets/images/language.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
 }
+
