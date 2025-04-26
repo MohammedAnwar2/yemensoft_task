a793f2820ff866e6aeefd8205adcdeae77aaacc6
MohammedAnwar2 <moanbm123@gmail.com>
2025-04-26 15:14:24 +0530

    Implement OrdersCubit and OrdersState with use cases for managing order bills

---
+++ lib/features/Orders/presentation/cubit/orders_cubit/orders_cubit.dart
@@ -0,0 +1,72 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:yemensoft_task/core/error/failures.dart';
+import 'package:yemensoft_task/features/orders/domain/entities/delivery_bill_entities.dart';
+import 'package:yemensoft_task/features/orders/domain/usecases/get_new_bills_usecase.dart';
+import 'package:yemensoft_task/features/orders/domain/usecases/get_other_bills_usecase.dart';
+import 'package:yemensoft_task/features/orders/domain/usecases/save_bills_usecase.dart';
+
+part 'orders_state.dart';
+
+class OrdersCubit extends Cubit<OrdersState> {
+  final GetNewBillsUsecase getNewBillsUsecase;
+  final GetOtherBillsUsecase getOtherBillsUsecase;
+  final SaveBillsUsecase saveBillsUsecase;
+  OrdersCubit({
+    required this.getNewBillsUsecase,
+    required this.getOtherBillsUsecase,
+    required this.saveBillsUsecase,
+  }) : super(OrdersInitial()){
+    saveBills();
+  }
+  Future<void> getNewBills() async {
+    emit(OrdersLoading());
+    final result = await getNewBillsUsecase.call();
+    result.fold(
+      (failure) {
+        if (failure is EmptyCacheFailure) {
+          emit(OrdersEmpty(failure.errorMessage));
+        } else {
+          emit(OrdersError(failure.errorMessage));
+        }
+      },
+      (bills) {
+        emit(OrdersLoaded(bills: bills));
+      },
+    );
+  }
+
+  Future<void> getOtherBills() async {
+    emit(OrdersLoading());
+    final result = await getOtherBillsUsecase.call();
+    result.fold(
+      (failure) {
+        if (failure is EmptyCacheFailure) {
+          emit(OrdersEmpty(failure.errorMessage));
+        } else {
+          emit(OrdersError(failure.errorMessage));
+        }
+      },
+      (bills) {
+        emit(OrdersLoaded(bills: bills));
+      },
+    );
+  }
+
+  Future<void> saveBills() async {
+    emit(OrdersLoading());
+    final result = await saveBillsUsecase.call();
+    result.fold(
+      (failure) {
+        if (failure is OfflineFailure) {
+          emit(OrdersOffline(failure.errorMessage));
+        } else {
+          emit(OrdersError(failure.errorMessage));
+        }
+      },
+      (_) {
+        emit(OrdersSavedLocaly());
+      },
+    );
+  }
+}
---
+++ lib/features/Orders/presentation/cubit/orders_cubit/orders_state.dart
@@ -0,0 +1,45 @@
+
+part of 'orders_cubit.dart';
+abstract class OrdersState extends Equatable {
+  const OrdersState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class OrdersInitial extends OrdersState {}
+
+class OrdersLoading extends OrdersState {}
+
+class OrdersLoaded extends OrdersState {
+  final List<OrderBillEntity> bills;
+  const OrdersLoaded({required this.bills});
+
+  @override
+  List<Object> get props => [bills];
+}
+
+class OrdersError extends OrdersState {
+  final String message;
+  const OrdersError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
+
+class OrdersEmpty extends OrdersState {
+  final String message;
+  const OrdersEmpty(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
+
+class OrdersSavedLocaly extends OrdersState {}
+class OrdersOffline extends OrdersState {
+  final String message;
+  const OrdersOffline(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
